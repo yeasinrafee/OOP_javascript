53e25c84081d87b8695773e40b3047081b62013c
yeasinrafee <mdyeasinrafi7@gmail.com>
2022-09-21 01:25:06 +0600

    Coding Challenge 01 done

--- script.js
+++ script.js
@@ -1,27 +1,57 @@
 "use strict";
 
-const Person = function (name, birthYear) {
-  this.name = name;
-  this.birthYear = birthYear;
+//Prototypal Inheritance:
+// const Person = function (name, birthYear) {
+//   this.name = name;
+//   this.birthYear = birthYear;
 
-  //Bad use:
-  //   this.age = function () {
-  //     console.log(`${2022 - this.birthYear}`);
-  //   };
+//   //Bad use:
+//   //   this.age = function () {
+//   //     console.log(`${2022 - this.birthYear}`);
+//   //   };
+// };
+// Person.prototype.age = function () {
+//   console.log(`${2022 - this.birthYear}`);
+// };
+// Person.prototype.species = "Homo sapiens";
+
+// const rafee = new Person("Rafee", 2000);
+// console.log(rafee);
+// rafee.age();
+// console.log(rafee.species);
+
+// console.log(Person.prototype.__proto__);
+// console.log(rafee.__proto__);
+
+// Person.prototype.__proto__.gender = "Male";
+
+// console.log(rafee.gender);
+// console.log(Person.prototype.__proto__.__proto__);
+
+// #Coding Challenge: 1:
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
 };
-Person.prototype.age = function () {
-  console.log(`${2022 - this.birthYear}`);
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`Speed is ${this.speed} km/h`);
 };
-Person.prototype.species = "Homo sapiens";
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`Speed is ${this.speed} km/h`);
+};
 
-const rafee = new Person("Rafee", 2000);
-console.log(rafee);
-rafee.age();
-console.log(rafee.species);
+const bmw = new Car("BMW", 120);
+const mercedes = new Car("Mercedes", 95);
 
-console.log(Person.prototype.__proto__);
-console.log(rafee.__proto__);
+console.log(bmw);
+console.log(mercedes);
 
-Person.prototype.__proto__.gender = "Male";
+bmw.accelerate();
+bmw.accelerate();
+bmw.brake();
 
-console.log(rafee.gender);
+mercedes.accelerate();
+mercedes.accelerate();
+mercedes.brake();
