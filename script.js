199c3c5c00218dcd67f804ddf66f7be8ae4ce43a
yeasinrafee <mdyeasinrafi7@gmail.com>
2022-09-21 02:08:21 +0600

    Javascript Module Pattern

--- script.js
+++ script.js
@@ -29,29 +29,59 @@
 // console.log(Person.prototype.__proto__.__proto__);
 
 // #Coding Challenge: 1:
-const Car = function (make, speed) {
-  this.make = make;
-  this.speed = speed;
-};
-Car.prototype.accelerate = function () {
-  this.speed += 10;
-  console.log(`Speed is ${this.speed} km/h`);
-};
-Car.prototype.brake = function () {
-  this.speed -= 5;
-  console.log(`Speed is ${this.speed} km/h`);
-};
+// const Car = function (make, speed) {
+//   this.make = make;
+//   this.speed = speed;
+// };
+// Car.prototype.accelerate = function () {
+//   this.speed += 10;
+//   console.log(`Speed is ${this.speed} km/h`);
+// };
+// Car.prototype.brake = function () {
+//   this.speed -= 5;
+//   console.log(`Speed is ${this.speed} km/h`);
+// };
 
-const bmw = new Car("BMW", 120);
-const mercedes = new Car("Mercedes", 95);
+// const bmw = new Car("BMW", 120);
+// const mercedes = new Car("Mercedes", 95);
 
-console.log(bmw);
-console.log(mercedes);
+// console.log(bmw);
+// console.log(mercedes);
 
-bmw.accelerate();
-bmw.accelerate();
-bmw.brake();
+// bmw.accelerate();
+// bmw.accelerate();
+// bmw.brake();
 
-mercedes.accelerate();
-mercedes.accelerate();
-mercedes.brake();
+// mercedes.accelerate();
+// mercedes.accelerate();
+// mercedes.brake();
+
+//Module Pattern:
+const ShoppingCart = (function () {
+  const products = [];
+  const price = 1250;
+  const shippingCharge = 150;
+
+  const addToCart = function (name, quantity) {
+    products.push({ name, quantity });
+    console.log(`Your product is ${name} of ${quantity} pieces`);
+  };
+  const displayCart = () => {
+    console.log(products);
+  };
+  const showBill = () => {
+    console.log(
+      `Price is ${price}, Shipping Charge: ${shippingCharge}, Total bill is ${
+        price + shippingCharge
+      }`
+    );
+  };
+
+  return { addToCart, displayCart, showBill };
+})();
+
+ShoppingCart.addToCart("Mango", 56);
+ShoppingCart.addToCart("Orange", 26);
+ShoppingCart.addToCart("Banana", 12);
+ShoppingCart.showBill();
+ShoppingCart.displayCart();
