b4d41582d16247d68271b85e099c421c295f0d89
yeasinrafee <mdyeasinrafi7@gmail.com>
2022-09-23 00:10:50 +0600

    Object.create added

--- object.create.js
+++ object.create.js
@@ -1,1 +1,15 @@
 "use strict";
+
+const DetailsProto = {
+  calcAge() {
+    console.log(2022 - this.birthYear);
+  },
+  info(name, birthYear) {
+    this.name = name;
+    this.birthYear = birthYear;
+  },
+};
+
+const rafee = Object.create(DetailsProto);
+rafee.info("Rafee", 2000);
+rafee.calcAge();
