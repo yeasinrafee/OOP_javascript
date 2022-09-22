65350862d414870ee622963cd12c51300e5f3d7c
yeasinrafee <mdyeasinrafi7@gmail.com>
2022-09-22 23:36:05 +0600

    Setters and Getters

--- ES6_classes.js
+++ ES6_classes.js
@@ -3,8 +3,8 @@
 //ES6 Classes:
 
 class Details {
-  constructor(name, year) {
-    this.name = name;
+  constructor(fullName, year) {
+    this.fullName = fullName;
     this.year = year;
   }
 
@@ -13,10 +13,27 @@ class Details {
   }
 
   showDetails() {
-    console.log(this.name, this.year);
+    console.log(this._fullName, this.year);
+  }
+
+  set fullName(name) {
+    if (name.includes(" ")) this._fullName = name;
+    else console.error("You have to give your full name");
+  }
+
+  get age() {
+    return 2022 - this.year;
+  }
+
+  get fullName() {
+    return this._fullName;
   }
 }
-
-const rafee = new Details("Rafee", 2000);
+const rafee = new Details("Rafee king", 2000);
 rafee.calcAge();
 rafee.showDetails();
+console.log(rafee.age);
+
+const aegon = new Details("Aegon the Conquarar", 1190);
+aegon.fullName = "Aegon Targaryen";
+console.log(aegon.fullName);
