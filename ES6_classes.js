18314fda5836a2c17fd707047c646bf3449ed930
yeasinrafee <mdyeasinrafi7@gmail.com>
2022-09-23 21:54:35 +0600

    Conding 2 done on getter and setter function

--- ES6_classes.js
+++ ES6_classes.js
@@ -37,11 +37,47 @@ class Details {
 Details.hey();
 
 const rafee = new Details("Rafee king", 2000);
-rafee.calcAge();
-rafee.showDetails();
-console.log(rafee.age);
+// rafee.calcAge();
+// rafee.showDetails();
+// console.log(rafee.age);
 // rafee.hey();
 
 const aegon = new Details("Aegon the Conquarar", 1190);
 aegon.fullName = "Aegon Targaryen";
-console.log(aegon.fullName);
+// console.log(aegon.fullName);
+
+// #Coding Challenge: 2:
+// class Car {
+//   constructor(make, speed) {
+//     this.make = make;
+//     this.speed = speed;
+//   }
+
+//   accelerate() {
+//     this.speed += 10;
+//     console.log(`Speed is ${this.speed} km/h`);
+//   }
+
+//   brake() {
+//     this.speed -= 5;
+//     console.log(`Speed is ${this.speed} km/h`);
+//   }
+
+//   get speedUS() {
+//     return this.speed / 1.6;
+//   }
+
+//   set speedUS(speed) {
+//     this.speed = speed * 1.6;
+//   }
+// }
+
+// const ford = new Car("Ford", 150);
+// ford.accelerate();
+// ford.accelerate();
+// ford.brake();
+// ford.accelerate();
+
+// console.log(ford.speedUS);
+// ford.speedUS = 50;
+// console.log(ford);
